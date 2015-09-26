13c5e12f7b4c3fb1e25c5163f93d49274968630b
jasenc <jasen.c8@gmail.com>
2015-09-25 22:18:14 -0700

    docs: ch-3.js complete

--- ch-3.js
+++ ch-3.js
@@ -6,6 +6,17 @@ Jasen Carroll
 Sept 25, 2015
 */
 
+/* Print Function
+
+I'm writing a function to print the functions.
+
+*/
+
+function print(name, func) {
+  console.log(name + ": " + func);
+  console.log("");
+}
+
 
 /* Minimum:
 
@@ -14,20 +25,64 @@ Write a function min that takes two arguments and returns their minimum.
 */
 
 function min(a, b) {
-  console.log("Minimum");
-  if (a < b) { console.log(a) }
-  else { console.log(b) }
-  console.log("");
-};
+  if (a < b) return a;
+  else return b;
+}
 
-min(0, 10);
-min(0, -10);
+console.log("#### Minimum ####");
+print("min(0, 10)", min(0, 10));
+print("min(0, -10)", min(0, -10));
 
-/* FizzBuzz
+/* Recursion
 
-Write a program that uses console.log to print all the numbers from 1 to 100,
-with three exceptions. For numbers divisible by 3, print "Fizz" instead of the
-number, for numbers divisible by 5 (and not 3), print "Buzz" instead, then for
-numbers divisable by both 3 and 5 print "FizzBuzz".
+Define a recursive function to determine whether a number is even or odd.
 
 */
+
+function isEven(num) {
+  if (num === 0) return "even";
+  else if (num === 1) return "odd";
+  else return isEven(num - 2);
+}
+
+console.log("#### Recursion ####");
+print("isEven(50)", isEven(50));
+print("isEven(75)", isEven(75));
+// The following call will provide "RangeError: Maximum call stack size exceeded"
+// print("Recursion (is even?)", isEven(-1));
+
+
+/* Bean Counting
+
+Write a function countBs that takes a string as its only argument and returns a
+number that indicates how many uppercase “B” characters are in the string.
+
+Next, write a function called countChar that behaves like countBs, except it
+takes a second argument that indicates the character that is to be counted
+(rather than counting only uppercase “B” characters).
+
+*/
+
+function countBs(string) {
+  var count = 0;
+  for (var i  = 0; i < string.length; i++) {
+    if (string[i] === "B") {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function countChar(string, char) {
+  var count = 0;
+  for (var i  = 0; i < string.length; i++) {
+    if (string[i] === char) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+console.log("#### Bean Counting ####");
+print("countBs('BBC')", countBs("BBC"));
+print("countChar('kakkerlak', 'k')", countChar("kakkerlak", "k"));
